2c35aea7eeb1d70f7a215eae4938c5c1f1b0f5aa
veviki <eshunvictricia840@gmail.com>
2024-07-08 05:21:25 -0700

    modified the index.html and src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+// when the user points on the explore button on the homepage 
+//it takes their name
+function promptName(){
+  let name =  prompt("what is your name");
+ let taste = document.querySelector(".taste");
+ taste.innerHTML = `welcome ${name}, Taste the Raw Goodness of Africa`
   
+}
+
+let product = document.getElementById("explore");
+ product.addEventListener("mouseenter", promptName);
