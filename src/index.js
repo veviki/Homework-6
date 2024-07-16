b3fbaef766006421aedb7e2425ad7e5b7ea66c4e
veviki <eshunvictricia840@gmail.com>
2024-07-16 10:10:14 -0700

    modified the index.html,pages/product.html,src/index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 // when the user points on the explore button on the homepage 
 //it takes their name
-function promptName(){
-  let name =  prompt("what is your name");
- let taste = document.querySelector(".taste");
- taste.innerHTML = `welcome ${name}, Taste the Raw Goodness of Africa`
+// function promptName(){
+//   let name =  prompt("what is your name");
+//  let taste = document.querySelector(".taste");
+//  taste.innerHTML = `welcome ${name}, Taste the Raw Goodness of Africa`
   
-}
+// }
 
-let product = document.getElementById("explore");
- product.addEventListener("mouseenter", promptName);
+// let product = document.getElementById("explore");
+//  product.addEventListener("mouseenter", promptName);
 
- //loading more images when i click
-function loadmore(){
-    let new_images = document.getElementById("new_images"); 
-    new_images.style.display = "block";
-    new_images.style.display = "flex";
-}
- let load= document.getElementById("load");
- load.addEventListener("click", loadmore);
+//  //loading more images when i click
+// function loadmore(){
+//     let new_images = document.getElementById("new_images"); 
+//     new_images.style.display = "block";
+//     new_images.style.display = "flex";
+// }
+//  let load= document.getElementById("load");
+//  load.addEventListener("click", loadmore);
  
