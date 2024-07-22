3e01f8e8787b8e9cf67c91ea5318e0ab0d4b59d8
veviki <eshunvictricia840@gmail.com>
2024-07-22 12:13:05 -0700

    added new items and images

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-// when the user points on the explore button on the homepage 
-//it takes their name
-// function promptName(){
-//   let name =  prompt("what is your name");
-//  let taste = document.querySelector(".taste");
-//  taste.innerHTML = `welcome ${name}, Taste the Raw Goodness of Africa`
+//when the user points on the explore button on the homepage //
+//it takes their name//
+function promptName(){
+  let name =  prompt("what is your name");
+ let taste = document.getElementById("text");
+ taste.innerHTML = `welcome ${name}, Taste the Raw Goodness of Africa`
+ let Browse =document.getElementById("browse");
+ Browse.innerHTML=`Welcome ${name},browse through the African Goodness`;
   
-// }
+}
 
-// let product = document.getElementById("explore");
-//  product.addEventListener("mouseenter", promptName);
+let product = document.getElementById("explore");
+ product.addEventListener("mouseenter", promptName);
 
-//  //loading more images when i click
-// function loadmore(){
-//     let new_images = document.getElementById("new_images"); 
-//     new_images.style.display = "block";
-//     new_images.style.display = "flex";
-// }
-//  let load= document.getElementById("load");
-//  load.addEventListener("click", loadmore);
+ //loading more images when i click
+function loadmore(){
+    let new_images = document.getElementById("new_images"); 
+    new_images.style.display = "block";
+    new_images.style.display = "flex";
+    let Hide=document.getElementById("hide");
+    Hide.style.display="flex";
+    let reveal=document.getElementById("load");
+    reveal.style.display="none";
+}
+ let load= document.getElementById("load");
+ load.addEventListener("click", loadmore);
+
+
+
+ 
  
